c12e423b5caa76d3a6498b398965aafe8483c16a
Klipron Howard <klipronhoward@gmail.com>
2024-12-06 17:12:42 -0500

    Updated 2024 Day 5: Simplified v1 of solution took out redundant parts.

--- 2024/day_05/main.py
+++ 2024/day_05/main.py
@@ -27,47 +27,41 @@ def get_input_file(year: int, day: int):
 
 
 def solve(path: Path, show_answer: Callable[[int, Any], None]):
-    text = path.read_text()
-    ordering_rules, pages_to_produce = text.split("\n\n")
-    ordering_rules = set(tuple(map(int, z.split("|"))) for z in ordering_rules.split())
-    pages_to_produce = tuple(
-        tuple(map(int, z.split(","))) for z in pages_to_produce.split()
-    )
+    top, bottom = path.read_text().split("\n\n")
+    ordering_rules = set(tuple(map(int, z.split("|"))) for z in top.split())
+    pages_to_produce = tuple(tuple(map(int, z.split(","))) for z in bottom.split())
+
     part1 = 0
     part2 = 0
     for pages in pages_to_produce:
-        page_rules = []
         for i, page in enumerate(pages):
             for next_page in pages[i + 1 :]:
-                page_rules.append((page, next_page))
-        if all(x in ordering_rules for x in page_rules):
-            part1 += pages[len(pages) // 2]
+                if (page, next_page) not in ordering_rules:
+                    break
+            else:
+                continue
+            break
         else:
-            rules = set()
-            for rule in ordering_rules:
-                if all(x in pages for x in rule):
-                    rules.add(rule)
-            new_ordering = []
-            for page in pages:
-                if not new_ordering:
-                    new_ordering.append(page)
-                    continue
-                for ii, ordered_page in enumerate(new_ordering):
-                    if (page, ordered_page) in rules:
-                        new_ordering.insert(ii, page)
-                        break
-                else:
-                    new_ordering.append(page)
-            part2 += new_ordering[len(pages) // 2]
+            part1 += pages[len(pages) // 2]
+            continue
+        ordered_pages = []
+        for page in pages:
+            for i, ordered_page in enumerate(ordered_pages):
+                if (page, ordered_page) in ordering_rules:
+                    ordered_pages.insert(i, page)
+                    break
+            else:
+                ordered_pages.append(page)
+        part2 += ordered_pages[len(ordered_pages) // 2]
 
     show_answer(1, part1)
     show_answer(2, part2)
 
 
 def solve_v2(path: Path, show_answer: Callable[[int, Any], None]):
-    parts = path.read_text().split("\n\n")
-    ordering_rules = set(tuple(map(int, x.split("|"))) for x in parts[0].split())
-    pages_to_produce = tuple(tuple(map(int, x.split(","))) for x in parts[1].split())
+    top, bottom = path.read_text().split("\n\n")
+    ordering_rules = set(tuple(map(int, x.split("|"))) for x in top.split())
+    pages_to_produce = tuple(tuple(map(int, x.split(","))) for x in bottom.split())
 
     part1 = 0
     part2 = 0
@@ -99,7 +93,7 @@ def main():
         f"Answer Part {part}: {answer or ''}", "=" * 80, sep="\n"
     )
     solve(INPUT_FILE, show_answer)
-    solve_v2(INPUT_FILE, show_answer)
+    # solve_v2(INPUT_FILE, show_answer)
 
 
 if __name__ == "__main__":
